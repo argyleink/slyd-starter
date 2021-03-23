309d2748568d9f22db77a324ef1308b0026012a6
Adam Argyle <atom@argyleink.com>
2021-03-23 14:15:21 -0700

    switch to vite

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,18 @@
 const postcssPresetEnv  = require('postcss-preset-env')
-const postcssImport     = require('postcss-import')
 const postcsseasings    = require('postcss-easings')
-const importUrl         = require('postcss-import-url')
 
 module.exports = {
   plugins: [
     postcsseasings(),
-    importUrl(),
-    postcssImport(),
     postcssPresetEnv({
       stage: 0,
-      browsers: [
-        '>0.25%',
-        'not ie 11',
-        'not op_mini all',
-      ],
+      features: {
+        'logical-properties-and-values': false, 
+        'prefers-color-scheme-query': false, 
+        'gap-properties': false,
+        'custom-properties': false,
+        'place-properties': false,
+      }
     }),
   ]
 }
